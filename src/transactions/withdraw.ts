68befab2e9c1e235fbcced603c2db324bf875866
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-07-06 06:19:14 +0000

    # PECORINO-22 ### Added - 一通りのAPIを追加。

--- src/transactions/withdraw.ts
+++ src/transactions/withdraw.ts
@@ -1,10 +1,11 @@
-// import * as pecorino from '@motionpicture/pecorino-domain';
+import * as pecorino from '@motionpicture/pecorino-domain';
 // tslint:disable-next-line:no-implicit-dependencies
-import { Callback, Context } from 'aws-lambda';
-
+import { Context } from 'aws-lambda';
 import { NO_CONTENT, OK } from 'http-status';
+import * as moment from 'moment';
 
 import { connectMongo } from '../connectMongo';
+import errorHandler from '../error';
 
 import * as createDebug from 'debug';
 const debug = createDebug('pecorino:*');
@@ -12,25 +13,49 @@ const debug = createDebug('pecorino:*');
 /**
  * 入金取引開始
  */
-export async function start(event: any, context: Context, callback: Callback) {
+export async function start(event: any, context: Context) {
     debug('event handled.', event);
     try {
         context.callbackWaitsForEmptyEventLoop = false;
+        const body = JSON.parse(event.body);
 
         await connectMongo();
 
-        callback(null, {
+        const accountRepo = new pecorino.repository.Account(pecorino.mongoose.connection);
+        const transactionRepo = new pecorino.repository.Transaction(pecorino.mongoose.connection);
+        const transaction = await pecorino.service.transaction.withdraw.start({
+            typeOf: pecorino.factory.transactionType.Withdraw,
+            agent: {
+                typeOf: body.agent.typeOf,
+                id: (body.agent.id !== undefined) ? body.agent.id : context.awsRequestId,
+                name: body.agent.name,
+                url: body.agent.url
+            },
+            recipient: {
+                typeOf: body.recipient.typeOf,
+                id: body.recipient.id,
+                name: body.recipient.name,
+                url: body.recipient.url
+            },
+            object: {
+                clientUser: <any>{},
+                amount: parseInt(body.amount, 10),
+                fromAccountNumber: body.fromAccountNumber,
+                notes: (body.notes !== undefined) ? body.notes : ''
+            },
+            expires: moment(body.expires).toDate()
+        })({ account: accountRepo, transaction: transactionRepo });
+
+        return {
             statusCode: OK,
-            body: JSON.stringify({
-                id: '12345'
-            })
-        });
+            body: JSON.stringify(transaction)
+        };
     } catch (error) {
-        callback(error);
+        return errorHandler(error);
     }
 }
 
-export async function confirm(event: any, context: Context, callback: Callback) {
+export async function confirm(event: any, context: Context) {
     debug('event handled.', event);
     try {
         context.callbackWaitsForEmptyEventLoop = false;
@@ -38,15 +63,21 @@ export async function confirm(event: any, context: Context, callback: Callback) 
 
         await connectMongo();
 
-        callback(null, {
+        const transactionRepo = new pecorino.repository.Transaction(pecorino.mongoose.connection);
+        await pecorino.service.transaction.withdraw.confirm({
+            transactionId: event.pathParameters.transactionId
+        })({ transaction: transactionRepo });
+        debug('transaction confirmed.');
+
+        return {
             statusCode: NO_CONTENT
-        });
+        };
     } catch (error) {
-        callback(error);
+        return errorHandler(error);
     }
 }
 
-export async function cancel(event: any, context: Context, callback: Callback) {
+export async function cancel(event: any, context: Context) {
     debug('event handled.', event);
     try {
         context.callbackWaitsForEmptyEventLoop = false;
@@ -54,10 +85,14 @@ export async function cancel(event: any, context: Context, callback: Callback) {
 
         await connectMongo();
 
-        callback(null, {
+        const transactionRepo = new pecorino.repository.Transaction(pecorino.mongoose.connection);
+        await transactionRepo.cancel(pecorino.factory.transactionType.Withdraw, event.pathParameters.transactionId);
+        debug('transaction canceled.');
+
+        return {
             statusCode: NO_CONTENT
-        });
+        };
     } catch (error) {
-        callback(error);
+        return errorHandler(error);
     }
 }
